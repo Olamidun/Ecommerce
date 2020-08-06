350780e77b4ef6f11c2e06c7c71ea1bb713ed2e0
Olamidun <kolapoolamidun@gmail.com>
2020-08-06 21:57:06 +0100

    corrected the signup alert message

--- users/views.py
+++ users/views.py
@@ -49,7 +49,8 @@ def register(request):
                 [email]
             )
             activation_mail.send()
-            messages.success(request, 'Your account has been created successfully, please check your mailto verify your account!')
+            messages.success(request, 'Your account has been created successfully,'
+                                      ' please check your mail to verify your account!')
             return redirect('users:login')
         elif password != password2:
             messages.success(request, 'Please ensure your passwords are the same.')
